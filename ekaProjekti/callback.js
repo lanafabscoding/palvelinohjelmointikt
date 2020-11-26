0635e886d59a7e1d4818ddd1dc88af9086f605f3
Kirsti Tikkanen <lanafabscoding@yeandex.com>
2020-11-26 20:29:26 +0200

    testejä tunnilla

---
+++ ekaProjekti/callback.js
@@ -0,0 +1,6 @@
+setTimeout(myCallback,5000);
+console.log("I'm not waiting for the callback");
+function myCallback() {
+    console.log("I'm a callback");
+}
+console.log("I'm not waiting for the callback, either");
