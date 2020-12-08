5534e277f168ffab3116ddb7d9997db1a1c2d429
Kirsti Tikkanen <lanafabscoding@yeandex.com>
2020-12-08 07:26:21 +0200

    yritin avata tietokantayhteyttä

--- mongoProjekti/server.js
+++ mongoProjekti/server.js
@@ -4,4 +4,10 @@ const mongoose = require("mongoose");
 const uri  = 'mongodb+srv://dbLana:moneyWellSpent!5@cluster0.umjne.mongodb.net/todolistdata?retryWrites=true&w=majority';
 //Muodostetaan yhteys tietokantaan
 mongoose.connect(uri, {useUnifiedTopology: true, useNewUrlParser: true});
+//Luodaan vakio tietokantayhteydelle
+const db = mongoose.connection;
+//Näytä ilmoitus, jos yhteys ok
+db.once("open", function () {
+    console.log("Tietokantayhteys avattu");
+});
 
