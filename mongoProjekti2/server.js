579d3a5a54d251fa3bf5e6fdf0e1c8d66163b333
Kirsti Tikkanen <lanafabscoding@yeandex.com>
2020-12-10 12:18:39 +0200

    melkein toimii

--- mongoProjekti2/server.js
+++ mongoProjekti2/server.js
@@ -1,5 +1,10 @@
 //Ota mongoose käyttöön
 const mongoose = require("mongoose");
+//Ota express käyttöön
+express = require("express");
+const app = express();
+//Ota users käyttöön
+const user = require("./user.js");
 //Luo connectionstringille vakio
 const uri = "mongodb+srv://dbLana:moneyWellSpent!5@cluster0.umjne.mongodb.net/todolistdata?retryWrites=true&w=majority";
 //Muodostetaan tietokantayhteys 
@@ -10,3 +15,17 @@ const db = mongoose.connection;
 db.once("open", function() {
     console.log("Tietokantayhteys avattu");
 });
+//Kirjoita get-funktio
+app.get('/users', function(req, res) {
+    //Hae käyttäjät tietokannasta
+    user.find({ }, function(err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    
+    })
+});
+//Laitetaan palvelin kuuntelemaan porttia 8080
+const server = app.listen(8080, function(){});
