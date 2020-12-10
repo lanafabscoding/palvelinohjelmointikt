30278594094bcc5b8dd198a9dbdc241594e1febb
Kirsti Tikkanen <lanafabscoding@yeandex.com>
2020-12-10 10:17:36 +0200

    ei onnistu yhdistäminen

--- mongoProjekti2/server.js
+++ mongoProjekti2/server.js
@@ -5,7 +5,7 @@ const uri = "mongodb+srv://dbLana:moneyWellSpent!5@cluster0.umjne.mongodb.net/to
 //Muodostetaan tietokantayhteys 
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true});
 //Luodaan vakio tietokantayhteydelle
-const db = mongoose.connect;
+const db = mongoose.connection;
 //Näyt ilmoitus jos yhteys ok
 db.once("open", function() {
     console.log("Tietokantayhteys avattu");
