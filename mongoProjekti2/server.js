7740708855872a7ee43e56451529f2e4340d5d1c
Kirsti Tikkanen <lanafabscoding@yeandex.com>
2020-12-10 10:13:36 +0200

    ei onnistu

---
+++ mongoProjekti2/server.js
@@ -0,0 +1,12 @@
+//Ota mongoose käyttöön
+const mongoose = require("mongoose");
+//Luo connectionstringille vakio
+const uri = "mongodb+srv://dbLana:moneyWellSpent!5@cluster0.umjne.mongodb.net/todolistdata?retryWrites=true&w=majority";
+//Muodostetaan tietokantayhteys 
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true});
+//Luodaan vakio tietokantayhteydelle
+const db = mongoose.connect;
+//Näyt ilmoitus jos yhteys ok
+db.once("open", function() {
+    console.log("Tietokantayhteys avattu");
+});
